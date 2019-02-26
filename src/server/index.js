a89b964d201959263a383ce2cf0aa2f5ad853938
vam <vamshop@gmail.com>
2019-02-26 20:29:12 +0300

    Admin path removed

--- src/server/index.js
+++ src/server/index.js
@@ -20,21 +20,9 @@ const STATIC_OPTIONS = {
 
 app.set('trust proxy', 1);
 app.use(helmet());
-app.get('/images/:entity/:id/:size/:filename', (req, res, next) => {
-	// A stub of image resizing (can be done with Nginx)
-	const newUrl = `/images/${req.params.entity}/${req.params.id}/${
-		req.params.filename
-	}`;
-	req.url = newUrl;
-	next();
-});
 app.use(express.static('public/content', STATIC_OPTIONS));
 app.use('/assets', express.static('theme/assets', STATIC_OPTIONS));
-app.use('/admin-assets', express.static('public/admin-assets', STATIC_OPTIONS));
 app.use('/sw.js', express.static('theme/assets/sw.js'));
-app.use('/admin', (req, res) => {
-	res.sendFile(ADMIN_INDEX_PATH);
-});
 app.get(
 	/^.+\.(jpg|jpeg|gif|png|bmp|ico|webp|svg|css|js|zip|rar|flv|swf|xls)$/,
 	(req, res) => {
