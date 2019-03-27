e09b36f7dddb0976d01b0cf23c7b645ba1668c46
vam <vamshop@gmail.com>
2019-03-27 11:26:07 +0300

    pz-client changed to cezerin2-client

--- src/server/pageRendering.js
+++ src/server/pageRendering.js
@@ -1,5 +1,5 @@
 import winston from 'winston';
-import CezerinClient from 'pz-client';
+import CezerinClient from 'cezerin2-client';
 import React from 'react';
 import { StaticRouter } from 'react-router';
 import { renderToString } from 'react-dom/server';
