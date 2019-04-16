8dd50eb85891dd337bdb67db66d80ac8fa3063fa
vam <vamshop@gmail.com>
2019-04-16 12:07:05 +0300

    ENV variables added for production config sample

--- config/store.production.js
+++ config/store.production.js
@@ -1,7 +1,7 @@
 // config used by store client side only
 module.exports = {
 	// store UI language
-	language: 'en',
+	language: process.env.LANGUAGE || 'en',
 
 	// used by Store (server side)
 	apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:3001/api/v1',
@@ -9,14 +9,14 @@ module.exports = {
 	// used by API to service assets
 	assetsBaseURL: process.env.ASSETS_BASE_URL || 'http://localhost:3001',
 
-	// used by Store (server and client side)
+	// used by Store (server side)
 	ajaxBaseUrl: process.env.AJAX_BASE_URL || 'http://localhost:3001/ajax',
 
 	storeListenPort: process.env.STORE_PORT || 3000,
 
 	// key to sign tokens
-	jwtSecretKey: '-',
+	jwtSecretKey: process.env.JWT_SECRET_KEY || '-',
 
 	// key to sign store cookies
-	cookieSecretKey: '-'
+	cookieSecretKey: process.env.COOKIE_SECRET_KEY || '-'
 };
