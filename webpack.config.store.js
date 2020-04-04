f53563c50aff971da68e7db648ec382004d2f52b
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-04-04 19:17:44 +0700

    Update webpack.config.store.js

--- webpack.config.store.js
+++ webpack.config.store.js
@@ -3,117 +3,104 @@ const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const WorkboxPlugin = require("workbox-webpack-plugin");
-import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate } from "workbox-strategies";
-import { NetworkOnly } from "workbox-strategies";
-import { NetworkFirst } from "workbox-strategies";
+const { GenerateSW } = require("workbox-webpack-plugin");
+module.exports = {
+	entry: {
+		app: ["@babel/polyfill", "./src/client/index.js"],
+		theme: ["theme"],
+	},
 
-registerRoute(
-	new RegExp("/(images|assets|admin-assets)/"),
-	new StaleWhileRevalidate()
-);
+	performance: {
+		hints: false,
+	},
 
-registerRoute(new RegExp("/api/"), new NetworkOnly()),
-	registerRoute(new RegExp("/ajax/payment_form_settings"), new NetworkOnly()),
-	registerRoute(new RegExp("/"), new NetworkFirst()),
-	(module.exports = {
-		entry: {
-			app: ["@babel/polyfill", "./src/client/index.js"],
-			theme: ["theme"],
-		},
+	output: {
+		publicPath: "/",
+		path: path.resolve(__dirname, "theme"),
+		filename: "assets/js/[name]-[chunkhash].js",
+		chunkFilename: "assets/js/[name]-[chunkhash].js",
+	},
 
-		performance: {
-			hints: false,
-		},
-
-		output: {
-			publicPath: "/",
-			path: path.resolve(__dirname, "theme"),
-			filename: "assets/js/[name]-[chunkhash].js",
-			chunkFilename: "assets/js/[name]-[chunkhash].js",
-		},
-
-		optimization: {
-			splitChunks: {
-				cacheGroups: {
-					vendor: {
-						chunks: "initial",
-						name: "theme",
-						test: "theme",
-						enforce: true,
-					},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					chunks: "initial",
+					name: "theme",
+					test: "theme",
+					enforce: true,
 				},
 			},
 		},
+	},
 
-		module: {
-			rules: [
-				{
-					test: /\.(js|jsx)$/,
-					exclude: /node_modules/,
-					use: {
-						loader: "babel-loader",
-						options: {
-							presets: ["@babel/env", "@babel/react"],
-							plugins: ["transform-class-properties"],
-						},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/env", "@babel/react"],
+						plugins: ["transform-class-properties"],
 					},
 				},
-				{
-					test: /\.css$/,
-					use: [
-						MiniCssExtractPlugin.loader,
-						{
-							loader: "css-loader",
-							options: {
-								modules: false,
-								importLoaders: true,
-							},
+			},
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							modules: false,
+							importLoaders: true,
 						},
-						"postcss-loader",
-					],
-				},
-				{
-					test: /\.s(c|a)ss$/,
-					use: [
-						MiniCssExtractPlugin.loader,
-						"css-loader",
-						"postcss-loader",
-						"sass-loader",
-					],
-				},
-			],
-		},
-
-		plugins: [
-			new CleanWebpackPlugin(),
-			new MiniCssExtractPlugin({
-				filename: "assets/css/bundle-[contenthash].css",
-				chunkFilename: "assets/css/bundle-[contenthash].css",
-			}),
-			new HtmlWebpackPlugin({
-				template: "theme/index.html",
-				inject: "body",
-				filename: "assets/index.html",
-			}),
-			new WorkboxPlugin.GenerateSW({
-				swDest: "assets/sw.js",
-				precacheManifestFilename: "assets/precache-manifest.[manifestHash].js",
-				clientsClaim: true,
-				skipWaiting: true,
-				exclude: [/\.html$/],
-			}),
-			new webpack.BannerPlugin({
-				banner: `Created: ${new Date().toUTCString()}`,
-				raw: false,
-				entryOnly: false,
-			}),
+					},
+					"postcss-loader",
+				],
+			},
+			{
+				test: /\.s(c|a)ss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"postcss-loader",
+					"sass-loader",
+				],
+			},
 		],
+	},
 
-		stats: {
-			children: false,
-			entrypoints: false,
-			modules: false,
-		},
-	});
+	plugins: [
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: "assets/css/bundle-[contenthash].css",
+			chunkFilename: "assets/css/bundle-[contenthash].css",
+		}),
+		new HtmlWebpackPlugin({
+			template: "theme/index.html",
+			inject: "body",
+			filename: "assets/index.html",
+		}),
+		new GenerateSW({
+			swDest: "assets/sw.js",
+			precacheManifestFilename: "assets/precache-manifest.[manifestHash].js",
+			clientsClaim: true,
+			skipWaiting: true,
+			exclude: [/\.html$/],
+		}),
+		new webpack.BannerPlugin({
+			banner: `Created: ${new Date().toUTCString()}`,
+			raw: false,
+			entryOnly: false,
+		}),
+	],
+
+	stats: {
+		children: false,
+		entrypoints: false,
+		modules: false,
+	},
+};
