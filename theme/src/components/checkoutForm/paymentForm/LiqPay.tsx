2287aa77c5f31a7fc0f4c61f57eda025dcffe496
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-06-12 10:37:17 +0700

    Update LiqPay.tsx

--- theme/src/components/checkoutForm/paymentForm/LiqPay.tsx
+++ theme/src/components/checkoutForm/paymentForm/LiqPay.tsx
@@ -1,30 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react"
 
-let scriptAdded = false;
-class PayPalButton extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  addScript = () => {
+let scriptAdded = false
+const PayPalButton = props => {
+  const addScript = () => {
     if (scriptAdded) {
-      this.executeScript();
-      return;
+      executeScript()
+      return
     }
 
-    const SCRIPT_URL = "https://static.liqpay.ua/libjs/checkout.js";
-    const container = document.body || document.head;
-    const script = document.createElement("script");
-    script.src = SCRIPT_URL;
+    const SCRIPT_URL = "https://static.liqpay.ua/libjs/checkout.js"
+    const container = document.body || document.head
+    const script = document.createElement("script")
+    script.src = SCRIPT_URL
     script.onload = () => {
-      this.executeScript();
-    };
-    container.appendChild(script);
-    scriptAdded = true;
-  };
+      executeScript()
+    }
+    container.appendChild(script)
+    scriptAdded = true
+  }
 
-  executeScript = () => {
-    const { formSettings, shopSettings, onPayment } = this.props;
+  const executeScript = () => {
+    const { formSettings, onPayment } = props
 
     LiqPayCheckout.init({
       data: formSettings.data,
@@ -33,35 +29,33 @@ class PayPalButton extends React.Component {
       embedTo: "#liqpay_checkout",
       mode: "embed",
     })
-      .on("liqpay.callback", (data) => {
+      .on("liqpay.callback", data => {
         if (data.status === "success") {
-          onPayment();
+          onPayment()
         }
       })
-      .on("liqpay.ready", (data) => {
+      .on("liqpay.ready", data => {
         // ready
       })
-      .on("liqpay.close", (data) => {
+      .on("liqpay.close", data => {
         // close
-      });
-  };
-
-  componentDidMount() {
-    this.addScript();
+      })
   }
 
-  componentDidUpdate() {
-    this.executeScript();
-  }
+  useEffect(() => {
+    addScript()
+  }, [])
 
-  render() {
-    const { formSettings, shopSettings, onPayment } = this.props;
+  //  componentDidUpdate() {
+  useEffect(() => {
+    executeScript()
+  })
 
-    return (
-      <>
-        <div id="liqpay_checkout" />
-      </>
-    );
-  }
+  return (
+    <>
+      <div id="liqpay_checkout" />
+    </>
+  )
 }
-export default PayPalButton;
+
+export default PayPalButton
