17ca2c62d71a886f37e8946ef560d9ef49166848
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-06-12 11:57:04 +0700

    Update index.tsx

--- theme/src/components/productDetails/index.tsx
+++ theme/src/components/productDetails/index.tsx
@@ -1,13 +1,11 @@
-import React from "react"
-import { NavLink } from "react-router-dom"
-import * as helper from "../../lib/helper"
-import { themeSettings, text } from "../../lib/settings"
+import React, { useState } from "react"
+import { themeSettings } from "../../lib/settings"
 import Disqus from "../comments/disqus"
 import ViewedProducts from "../products/viewed"
-import Breadcrumbs from "./breadcrumbs"
-import DiscountCountdown from "./discountCountdown"
 import AddToCartButton from "./addToCartButton"
 import Attributes from "./attributes"
+import Breadcrumbs from "./breadcrumbs"
+import DiscountCountdown from "./discountCountdown"
 import Gallery from "./gallery"
 import Options from "./options"
 import Price from "./price"
@@ -22,66 +20,55 @@ const Description = ({ description }) => (
   />
 )
 
-class ProductDetails extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      selectedOptions: {},
-      selectedVariant: null,
-      isAllOptionsSelected: false,
-      quantity: 1,
-    }
+const ProductDetails = props => {
+  const [selectedOptions, setSelectedOptions] = useState({})
+  const [selectedVariant, setSelectedVariant] = useState(null)
+  const [isAllOptionsSelected, setIsAllOptionsSelected] = useState(false)
+  const [quantity, setQuantity] = useState(1)
 
-    this.onOptionChange = this.onOptionChange.bind(this)
-    this.findVariantBySelectedOptions = this.findVariantBySelectedOptions.bind(
-      this
-    )
-    this.addToCart = this.addToCart.bind(this)
-    this.checkSelectedOptions = this.checkSelectedOptions.bind(this)
-  }
+  //   onOptionChange = onOptionChange.bind(this)
+  //   findVariantBySelectedOptions = findVariantBySelectedOptions.bind(
+  //     this
+  //   )
+  //   const addToCart = addToCart.bind(this)
+  //   const checkSelectedOptions = checkSelectedOptions.bind(this)
+  // }
 
-  onOptionChange(optionId, valueId) {
-    const { selectedOptions } = this.state
-
+  function onOptionChange(optionId, valueId) {
     if (valueId === "") {
       delete selectedOptions[optionId]
     } else {
       selectedOptions[optionId] = valueId
     }
 
-    this.setState({ selectedOptions })
-    this.findVariantBySelectedOptions()
-    this.checkSelectedOptions()
+    setSelectedOptions
+    findVariantBySelectedOptions()
+    checkSelectedOptions()
   }
 
-  findVariantBySelectedOptions() {
-    const { selectedOptions } = this.state
-    const { product } = this.props
+  function findVariantBySelectedOptions() {
+    const { product } = props
     for (const variant of product.variants) {
       const variantMutchSelectedOptions = variant.options.every(
         variantOption =>
           selectedOptions[variantOption.option_id] === variantOption.value_id
       )
       if (variantMutchSelectedOptions) {
-        this.setState({ selectedVariant: variant })
+        setSelectedVariant(variant)
         return
       }
     }
 
-    this.setState({ selectedVariant: null })
+    setSelectedVariant(null)
   }
 
-  setQuantity = quantity => {
-    this.setState({ quantity })
-  }
-
-  addToCart() {
-    const { product, addCartItem } = this.props
-    const { selectedVariant, quantity } = this.state
+  function addToCart() {
+    const { product, addCartItem } = props
 
     const item = {
       product_id: product.id,
       quantity,
+      variant_id,
     }
 
     if (selectedVariant) {
@@ -91,125 +78,119 @@ class ProductDetails extends React.Component {
     addCartItem(item)
   }
 
-  checkSelectedOptions() {
-    const { selectedOptions } = this.state
-    const { product } = this.props
+  function checkSelectedOptions() {
+    const { product } = props
 
     const allOptionsSelected =
       Object.keys(selectedOptions).length === product.options.length
-    this.setState({ isAllOptionsSelected: allOptionsSelected })
+    setIsAllOptionsSelected(allOptionsSelected)
   }
 
-  render() {
-    const { product, settings, categories } = this.props
-    const { selectedVariant, isAllOptionsSelected } = this.state
-    const maxQuantity =
-      product.stock_status === "discontinued"
-        ? 0
-        : product.stock_backorder
-        ? themeSettings.maxCartItemQty
-        : selectedVariant
-        ? selectedVariant.stock_quantity
-        : product.stock_quantity
+  const { product, settings, categories } = props
+  const maxQuantity =
+    product.stock_status === "discontinued"
+      ? 0
+      : product.stock_backorder
+      ? themeSettings.maxCartItemQty
+      : selectedVariant
+      ? selectedVariant.stock_quantity
+      : product.stock_quantity
 
-    if (product) {
-      return (
-        <>
-          <section className="section section-product">
-            <div className="container">
-              <div className="columns">
-                <div className="column is-7">
-                  {themeSettings.show_product_breadcrumbs && (
-                    <Breadcrumbs product={product} categories={categories} />
-                  )}
-                  <Gallery images={product.images} />
-                </div>
-                <div className="column is-5">
-                  <div className="content">
-                    <Tags tags={product.tags} />
-                    <h1 className="title is-4 product-name">{product.name}</h1>
-                    <Price
+  if (product) {
+    return (
+      <>
+        <section className="section section-product">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-7">
+                {themeSettings.show_product_breadcrumbs && (
+                  <Breadcrumbs product={product} categories={categories} />
+                )}
+                <Gallery images={product.images} />
+              </div>
+              <div className="column is-5">
+                <div className="content">
+                  <Tags tags={product.tags} />
+                  <h1 className="title is-4 product-name">{product.name}</h1>
+                  <Price
+                    product={product}
+                    variant={selectedVariant}
+                    isAllOptionsSelected={isAllOptionsSelected}
+                    settings={settings}
+                  />
+
+                  {themeSettings.show_discount_countdown &&
+                    product.on_sale === true && (
+                      <DiscountCountdown product={product} />
+                    )}
+
+                  <Options
+                    options={product.options}
+                    onChange={onOptionChange}
+                  />
+                  <Quantity maxQuantity={maxQuantity} onChange={setQuantity} />
+                  <div className="button-addtocart">
+                    <AddToCartButton
                       product={product}
                       variant={selectedVariant}
+                      addCartItem={addToCart}
                       isAllOptionsSelected={isAllOptionsSelected}
-                      settings={settings}
                     />
-
-                    {themeSettings.show_discount_countdown &&
-                      product.on_sale === true && (
-                        <DiscountCountdown product={product} />
-                      )}
-
-                    <Options
-                      options={product.options}
-                      onChange={this.onOptionChange}
-                    />
-                    <Quantity
-                      maxQuantity={maxQuantity}
-                      onChange={this.setQuantity}
-                    />
-                    <div className="button-addtocart">
-                      <AddToCartButton
-                        product={product}
-                        variant={selectedVariant}
-                        addCartItem={this.addToCart}
-                        isAllOptionsSelected={isAllOptionsSelected}
-                      />
-                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </section>
+          </div>
+        </section>
 
-          <section className="section section-product-description">
-            <div className="container">
-              <div className="content">
-                <div className="columns">
-                  <div className="column is-7">
-                    <Description description={product.description} />
-                  </div>
-                  <div className="column is-5">
-                    <Attributes attributes={product.attributes} />
-                  </div>
+        <section className="section section-product-description">
+          <div className="container">
+            <div className="content">
+              <div className="columns">
+                <div className="column is-7">
+                  <Description description={product.description} />
+                </div>
+                <div className="column is-5">
+                  <Attributes attributes={product.attributes} />
                 </div>
               </div>
             </div>
-          </section>
+          </div>
+        </section>
 
-          <RelatedProducts
+        <RelatedProducts
+          settings={settings}
+          addCartItem={addToCart}
+          ids={product.related_product_ids}
+          limit={10}
+        />
+
+        {themeSettings.show_viewed_products && (
+          <ViewedProducts
             settings={settings}
-            addCartItem={this.addToCart}
-            ids={product.related_product_ids}
-            limit={10}
+            addCartItem={addToCart}
+            product={product}
+            limit={themeSettings.limit_viewed_products || 4}
           />
+        )}
 
-          {themeSettings.show_viewed_products && (
-            <ViewedProducts
-              settings={settings}
-              addCartItem={this.addToCart}
-              product={product}
-              limit={themeSettings.limit_viewed_products || 4}
-            />
+        {themeSettings.disqus_shortname &&
+          themeSettings.disqus_shortname !== "" && (
+            <section className="section">
+              <div className="container">
+                <Disqus
+                  shortname={themeSettings.disqus_shortname}
+                  identifier={product.id}
+                  title={product.name}
+                  url={product.url}
+                />
+              </div>
+            </section>
           )}
-
-          {themeSettings.disqus_shortname &&
-            themeSettings.disqus_shortname !== "" && (
-              <section className="section">
-                <div className="container">
-                  <Disqus
-                    shortname={themeSettings.disqus_shortname}
-                    identifier={product.id}
-                    title={product.name}
-                    url={product.url}
-                  />
-                </div>
-              </section>
-            )}
-        </>
-      )
-    }
-    return null
+      </>
+    )
   }
+  return null
 }
+
 export default ProductDetails
