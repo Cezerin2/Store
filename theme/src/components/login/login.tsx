58ade0340d1890bad4b50a83b5895a17e60c22a1
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-06-12 11:34:01 +0700

    Update login.tsx

--- theme/src/components/login/login.tsx
+++ theme/src/components/login/login.tsx
@@ -1,25 +1,25 @@
-import Lscache from "lscache";
-import React from "react";
-import { Link, NavLink, Redirect } from "react-router-dom";
-import { Field, reduxForm } from "redux-form";
-import { text } from "../../lib/settings";
+import Lscache from "lscache"
+import React, { useState } from "react"
+import { Link, NavLink, Redirect } from "react-router-dom"
+import { Field, reduxForm } from "redux-form"
+import { text } from "../../lib/settings"
 
-const validateRequired = (value) =>
-  value && value.length > 0 ? undefined : text.required;
+const validateRequired = value =>
+  value && value.length > 0 ? undefined : text.required
 
-const validateEmail = (value) =>
+const validateEmail = value =>
   value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
     ? text.emailInvalid
-    : undefined;
+    : undefined
 
 const ReadOnlyField = ({ name, value }) => (
   <div className="checkout-field-preview">
     <div className="name">{name}</div>
     <div className="value">{value}</div>
   </div>
-);
+)
 
-const InputField = (field) => (
+const InputField = field => (
   <div className={field.className}>
     <label htmlFor={field.id}>
       {field.label}
@@ -35,200 +35,185 @@ const InputField = (field) => (
       className={field.meta.touched && field.meta.error ? "invalid" : ""}
     />
   </div>
-);
+)
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      unauthorized: false,
-    };
+const Login = props => {
+  const [unauthorized, setUnauthorized] = useState(false)
+
+  function verifyAuth() {
+    setUnauthorized(true)
   }
 
-  verifyAuth() {
-    this.setState({ unauthorized: true });
+  const getField = fieldName => {
+    const fields = props.checkoutFields || []
+    const field = fields.find(item => item.name === fieldName)
+    return field
   }
 
-  getField = (fieldName) => {
-    const fields = this.props.checkoutFields || [];
-    const field = fields.find((item) => item.name === fieldName);
-    return field;
-  };
+  const getFieldStatus = fieldName => {
+    const field = getField(fieldName)
+    return field && field.status ? field.status : "required"
+  }
 
-  getFieldStatus = (fieldName) => {
-    const field = this.getField(fieldName);
-    return field && field.status ? field.status : "required";
-  };
+  const isFieldOptional = fieldName => getFieldStatus(fieldName) === "optional"
 
-  isFieldOptional = (fieldName) =>
-    this.getFieldStatus(fieldName) === "optional";
+  const isFieldHidden = fieldName => getFieldStatus(fieldName) === "hidden"
 
-  isFieldHidden = (fieldName) => this.getFieldStatus(fieldName) === "hidden";
-
-  getFieldValidators = (fieldName) => {
-    const isOptional = this.isFieldOptional(fieldName);
-    const validatorsArray = [];
+  const getFieldValidators = fieldName => {
+    const isOptional = isFieldOptional(fieldName)
+    const validatorsArray = []
     if (!isOptional) {
-      validatorsArray.push(validateRequired);
+      validatorsArray.push(validateRequired)
     }
     if (fieldName === "email") {
-      validatorsArray.push(validateEmail);
+      validatorsArray.push(validateEmail)
     }
 
-    return validatorsArray;
-  };
+    return validatorsArray
+  }
 
-  getFieldPlaceholder = (fieldName) => {
-    const field = this.getField(fieldName);
+  const getFieldPlaceholder = fieldName => {
+    const field = getField(fieldName)
     return field && field.placeholder && field.placeholder.length > 0
       ? field.placeholder
-      : "";
-  };
+      : ""
+  }
 
-  getFieldLabelText = (fieldName) => {
-    const field = this.getField(fieldName);
+  const getFieldLabelText = fieldName => {
+    const field = getField(fieldName)
     if (field && field.label && field.label.length > 0) {
-      return field.label;
+      return field.label
     }
     switch (fieldName) {
       case "email":
-        return text.email;
-        break;
+        return text.email
       case "password":
-        return text.password;
-        break;
+        return text.password
       default:
-        return "Unnamed field";
+        return "Unnamed field"
     }
-  };
+  }
 
-  getFieldLabel = (fieldName) => {
-    const labelText = this.getFieldLabelText(fieldName);
-    return this.isFieldOptional(fieldName)
+  const getFieldLabel = fieldName => {
+    const labelText = getFieldLabelText(fieldName)
+    return isFieldOptional(fieldName)
       ? `${labelText} (${text.optional})`
-      : labelText;
-  };
+      : labelText
+  }
 
-  render() {
-    const {
-      handleSubmit,
-      customerProperties,
-      cartlayerBtnInitialized,
-    } = this.props;
+  const { handleSubmit } = props
 
-    if (
-      this.props.customerProperties !== undefined &&
-      Lscache.get("auth_data") !== null
-    ) {
-      return (
-        <Redirect
-          to={{
-            pathname: "/customer-account",
-          }}
-        />
-      );
-    }
+  if (
+    props.customerProperties !== undefined &&
+    Lscache.get("auth_data") !== null
+  ) {
+    return (
+      <Redirect
+        to={{
+          pathname: "/customer-account",
+        }}
+      />
+    )
+  }
 
-    if (this.props.customerProperties !== undefined) {
-      if (!this.props.customerProperties.authenticated) {
-        if (!this.state.unauthorized) {
-          this.verifyAuth();
-        }
+  if (props.customerProperties !== undefined) {
+    if (!props.customerProperties.authenticated) {
+      if (!unauthorized) {
+        verifyAuth()
       }
     }
+  }
 
-    const inputClassName = "login-input-field";
-    const titleClassName = "login-title";
-    const loginButtonClass = "account-button button";
-    const loginSectionGuest = "login-section-guest";
-    const errorAlertText = "error-alert-text";
-    const loginForm = this.props.cartlayerBtnInitialized !== undefined &&
-      this.props.cartlayerBtnInitialized
+  const inputClassName = "login-input-field"
+  const titleClassName = "login-title"
+  const loginButtonClass = "account-button button"
+  const loginSectionGuest = "login-section-guest"
+  const errorAlertText = "error-alert-text"
+  const loginForm =
+    props.cartlayerBtnInitialized !== undefined && props.cartlayerBtnInitialized
       ? "login-form login-form login-form-with-guest"
-      : "login-form";
+      : "login-form"
 
-    return (
-      <div className="login-container">
-        {this.props.cartlayerBtnInitialized !== undefined &&
-          this.props.cartlayerBtnInitialized && (
-            <div className="login-guest-checkout">
-              <div className={loginSectionGuest}>
-                <h2>{text.continue_guest_headline}</h2>
-                <p>{text.continue_guest_text}</p>
-                <div className="login-button-wrap">
-                  <NavLink
-                    className="button loginButtonClass"
-                    style={{ textTransform: "uppercase" }}
-                    to="/checkout"
-                  >
-                    {text.proceedToCheckout}
-                  </NavLink>
-                </div>
+  return (
+    <div className="login-container">
+      {props.cartlayerBtnInitialized !== undefined &&
+        props.cartlayerBtnInitialized && (
+          <div className="login-guest-checkout">
+            <div className={loginSectionGuest}>
+              <h2>{text.continue_guest_headline}</h2>
+              <p>{text.continue_guest_text}</p>
+              <div className="login-button-wrap">
+                <NavLink
+                  className="button loginButtonClass"
+                  style={{ textTransform: "uppercase" }}
+                  to="/checkout"
+                >
+                  {text.proceedToCheckout}
+                </NavLink>
               </div>
             </div>
+          </div>
+        )}
+      <form onSubmit={handleSubmit} className={loginForm}>
+        <div className="login-section">
+          <h2 className={titleClassName}>{text.login_title}</h2>
+          {props.customerProperties !== undefined &&
+          props.customerProperties.loggedin_failed ? (
+            <p className={errorAlertText}>{text.login_failed}</p>
+          ) : (
+            ""
           )}
-        <form onSubmit={handleSubmit} className={loginForm}>
-          <div className="login-section">
-            <h2 className={titleClassName}>{text.login_title}</h2>
-            {this.props.customerProperties !== undefined &&
-              this.props.customerProperties.loggedin_failed
-              ? (
-                <p className={errorAlertText}>{text.login_failed}</p>
-              )
-              : (
-                ""
-              )}
-            {!this.isFieldHidden("email") && (
-              <Field
-                className={inputClassName}
-                name="email"
-                id="customer.email"
-                component={InputField}
-                type="email"
-                label={this.getFieldLabel("email")}
-                validate={this.getFieldValidators("email")}
-                placeholder={this.getFieldPlaceholder("email")}
-              />
-            )}
+          {!isFieldHidden("email") && (
+            <Field
+              className={inputClassName}
+              name="email"
+              id="customer.email"
+              component={InputField}
+              type="email"
+              label={getFieldLabel("email")}
+              validate={getFieldValidators("email")}
+              placeholder={getFieldPlaceholder("email")}
+            />
+          )}
 
-            {!this.isFieldHidden("password") && (
-              <Field
-                className={inputClassName}
-                name="password"
-                id="customer.password"
-                component={InputField}
-                type="password"
-                label={this.getFieldLabel("password")}
-                validate={this.getFieldValidators("password")}
-                placeholder={this.getFieldPlaceholder("password")}
-              />
-            )}
-            <div className="login-link-wrap">
-              <Link to="/forgot-password">{text.forgot_password}</Link>
-            </div>
+          {!isFieldHidden("password") && (
+            <Field
+              className={inputClassName}
+              name="password"
+              id="customer.password"
+              component={InputField}
+              type="password"
+              label={getFieldLabel("password")}
+              validate={getFieldValidators("password")}
+              placeholder={getFieldPlaceholder("password")}
+            />
+          )}
+          <div className="login-link-wrap">
+            <Link to="/forgot-password">{text.forgot_password}</Link>
+          </div>
+          <div className="login-button-wrap">
+            <button type="submit" className={loginButtonClass}>
+              {text.login}
+            </button>
+          </div>
+
+          <NavLink className="logo-image" to="/register">
             <div className="login-button-wrap">
-              <button type="submit" className={loginButtonClass}>
-                {text.login}
+              <button
+                type="button"
+                className={loginButtonClass}
+                onClick={switchRegister}
+              >
+                {text.register}
               </button>
             </div>
-
-            <NavLink className="logo-image" to="/register">
-              <div className="login-button-wrap">
-                <button
-                  type="button"
-                  className={loginButtonClass}
-                  onClick={this.switchRegister}
-                >
-                  {text.register}
-                </button>
-              </div>
-            </NavLink>
-          </div>
-        </form>
-      </div>
-    );
-  }
+          </NavLink>
+        </div>
+      </form>
+    </div>
+  )
 }
+
 export default reduxForm({
   form: "Login",
-})(Login);
+})(Login)
