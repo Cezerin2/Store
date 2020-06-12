e9a34a82bfe38361f69f1d66b0cf6fec50e4228c
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-06-12 12:00:28 +0700

    Update relatedProducts.tsx

--- theme/src/components/productDetails/relatedProducts.tsx
+++ theme/src/components/productDetails/relatedProducts.tsx
@@ -1,34 +1,33 @@
-import React, { Fragment } from "react"
-import { themeSettings, text } from "../../lib/settings"
+import React from "react"
+import { text, themeSettings } from "../../lib/settings"
 import CustomProducts from "../products/custom"
 
-class RelatedProducts extends React.PureComponent {
-  render() {
-    const { ids, settings, addCartItem, limit } = this.props
-    if (ids && ids.length > 0) {
-      const title =
-        themeSettings.related_products_title &&
-        themeSettings.related_products_title.length > 0
-          ? themeSettings.related_products_title
-          : text.relatedProducts
+const RelatedProducts = props => {
+  const { ids, settings, addCartItem, limit } = props
+  if (ids && ids.length > 0) {
+    const title =
+      themeSettings.related_products_title &&
+      themeSettings.related_products_title.length > 0
+        ? themeSettings.related_products_title
+        : text.relatedProducts
 
-      return (
-        <section className="section section-product-related">
-          <div className="container">
-            <div className="title is-4 has-text-centered">{title}</div>
-            <CustomProducts
-              ids={ids}
-              sort={null}
-              limit={limit}
-              isCentered
-              settings={settings}
-              addCartItem={addCartItem}
-            />
-          </div>
-        </section>
-      )
-    }
-    return null
+    return (
+      <section className="section section-product-related">
+        <div className="container">
+          <div className="title is-4 has-text-centered">{title}</div>
+          <CustomProducts
+            ids={ids}
+            sort={null}
+            limit={limit}
+            isCentered
+            settings={settings}
+            addCartItem={addCartItem}
+          />
+        </div>
+      </section>
+    )
   }
+  return null
 }
+
 export default RelatedProducts
